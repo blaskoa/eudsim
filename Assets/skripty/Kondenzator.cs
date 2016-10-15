c4bce55e037968a8a95df536ad3fffb0ed016713
Tomas Liscak <thomashh55@gmail.com>
2016-10-15 20:44:33 +0200

    Prvy navrh suciastok

---
+++ Assets/skripty/Kondenzator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kondenzator : MonoBehaviour {
+    float kapacita;
+    float maxNapetie;
+    Spojovnik[] spojovnici;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/skripty/Spinac.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinac : MonoBehaviour {
+    Spojovnik[] spojovnici;
+    bool zapnuty;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
