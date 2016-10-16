8762e8a09314664a560657cf99e9351d6442874f
Branislav Makan <branislav.makan1994@gmail.com>
2016-10-16 21:47:27 +0200

    "Create" implemented. When dragging a GameObject tagged as "ToolboxItem" a new instance of that object will be created to be dragged around freely and worked with.

--- Assets/Scripts/GenericScripts/Draggable.cs
+++ Assets/Scripts/GenericScripts/Draggable.cs
@@ -8,18 +8,32 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     private Vector2 mousePos;
     private Vector2 itemPos;
 
+    // Item we're dragging.
+    private GameObject draggingItem;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         // Setting starting posiitons.
         mousePos = Camera.main.ScreenToWorldPoint(eventData.position);
         itemPos = this.transform.position;
+
+        // ToolboxItem tagged GameObjects are used to generate new instances for the working panel.
+        if (this.gameObject.tag == "ToolboxItem")
+        {
+            draggingItem = Instantiate(this.gameObject);
+            draggingItem.tag = "Untagged";
+        }
+        else
+        {
+            draggingItem = this.gameObject;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         //Moving the item with the mouse.
         Vector2 mouseDiff = (Vector2)Camera.main.ScreenToWorldPoint(eventData.position) - mousePos;
-        this.transform.position = itemPos + mouseDiff;
+        draggingItem.transform.position = itemPos + mouseDiff;
     }
 
     public void OnEndDrag(PointerEventData eventData)
