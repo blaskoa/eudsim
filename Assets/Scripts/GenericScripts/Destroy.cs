2a8f3b67777e98e8ffd117b5f213e4b3f61a8111
Branislav Makan <branislav.makan1994@gmail.com>
2016-10-29 09:25:39 +0200

    Selected objects can be deleted.

---
+++ Assets/Scripts/GenericScripts/Destroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SelectObject.selectedObject != null && this.gameObject == SelectObject.selectedObject)
+        {
+            if (Input.GetKey(KeyCode.Delete))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+	}
+}
