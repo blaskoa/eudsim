037dfd7c889a3d58c377157dd46020dc85c444c2
Branislav Makan <branislav.makan1994@gmail.com>
2016-10-29 10:28:21 +0200

    Deselection implemented.

---
+++ Assets/Scripts/GenericScripts/Deselect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deselect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // When left mouse button is pressed...
+    void OnMouseDown()
+    {
+        SelectObject.selectedObject.transform.FindChild("SelectionBox").GetComponent<SpriteRenderer>().enabled = false;
+        SelectObject.selectedObject = null;
+    }
+}
