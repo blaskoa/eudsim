cf2cef0e4d188ad0955986e8e63e128884c5bc69
Marek Matula <marekmatula126@gmail.com>
2016-10-30 21:52:00 +0100

    pridana trieda GUICircuit a Zapracovavanie dll kniznice do objektov

--- Assets/Scripts/Electric components/Accumulator.cs
+++ Assets/Scripts/Electric components/Accumulator.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Assets.Skripts.Interfaces;
+//using Assets.Skripts.Circuit;
+using SharpCircuit;
 
 public class Accumulator : MonoBehaviour, ComponentInterface {
     Connector[] connectors;
+    
+    VoltageInput myComponent = GUICircuit.sim.Create<VoltageInput>(Voltage.WaveType.DC);
+    Resistor myComponent2 = GUICircuit.sim.Create<Resistor>();
 
     // Use this for initialization
     void Start () {
