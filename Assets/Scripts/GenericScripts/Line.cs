a48474dd69f627fd2b111112e4cad7aa12c9bd12
Martin Cvicela <mato155@gmail.com>
2016-11-04 11:54:09 +0100

    vykreslovanie ciar medzi objektami connector, ulozenie prepojeni v liste, ciary sa hybu spolu s objektami, jednoducha moznost napojenia na suciastky

---
+++ Assets/Scripts/GenericScripts/Line.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Line : MonoBehaviour
+{
+
+    LineRenderer line;
+    Transform tr1;
+    Transform tr2;
+    public string name1;
+    public string name2;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tr1 = GameObject.Find(name1).transform;
+        tr2 = GameObject.Find(name2).transform;
+        line = GetComponent<LineRenderer>();
+        line.SetVertexCount(2);
+        line.SetPosition(0, tr1.transform.position);
+        line.SetPosition(1, tr2.transform.position);
+	}
+}
---
+++ Assets/kreslenie_ciar(samostatna_scena_na_predvedenie)/Assets/Scripts/CounterForConnect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CounterForConnect {
+
+    public static int count = 0;
+    public static GameObject previous;
+
+    public int getCount()
+    {
+        return count;
+    }
+
+    public void increment()
+    {
+        count++;
+    }
+
+    public void decrement()
+    {
+        count--;
+    }
+
+    public void resetCount()
+    {
+        count = 0;
+    }
+
+    public void setPrevious(GameObject prev) {
+        previous = prev;
+    }
+
+    public GameObject getPrevious()
+    {
+        return previous;
+    }
+}
