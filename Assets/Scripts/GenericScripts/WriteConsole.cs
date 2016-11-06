de432d6cfc81fd2ee1e69dfd1701347c13e14a67
Bloodxy <gulis2505@gmail.com>
2016-11-06 03:57:31 +0100

    Pridaná funkcionalita debug logu (konzoly). Výpis Hello World. Pridaný scrollrect pre konzolu.

---
+++ Assets/Scripts/GenericScripts/WriteConsole.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+ 
+public class WriteConsole : MonoBehaviour {
+ 
+    public Text output;
+	public string message;
+	
+    void Start () {
+        //output = gameObject.GetComponent<Text>();
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+		SubmitMessage(message);
+    }
+ 
+    private void SubmitMessage(string arg0)
+    {
+        //string currentText = input.text; //maybe add ToString()?
+        //string newText = currentText + "\n" + arg0;
+        output.text += arg0 + "\n";
+    }     
+ 
+}
