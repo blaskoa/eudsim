5ce5684860382e2a85de7fa0e589136eda580192
blaskoa <adam.blasko1@gmail.com>
2016-11-07 00:05:56 +0100

    Localization - added resource file (containing Slovak and English), renamed all text elements in scene - every text is loaded from resource file, added cycling of current locales to new project button on toolbar

---
+++ Assets/Scripts/Localization/Localization.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Assets.Scripts.Localization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Localization : MonoBehaviour
+{
+    private ResourceReader _resourceReader;
+	// Use this for initialization
+	void Start ()
+    {
+	    _resourceReader = new ResourceReader(Path.Combine(Application.dataPath, ResourceReader.ResourceFileName), Application.systemLanguage);
+        GetComponent<Text>().text = _resourceReader.GetResource(gameObject.name);
+    }
+
+    public void ChangeLanguage(SystemLanguage language)
+    {
+        _resourceReader = new ResourceReader(Path.Combine(Application.dataPath, ResourceReader.ResourceFileName), language);
+        GetComponent<Text>().text = _resourceReader.GetResource(gameObject.name);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/Scripts/Localization/ResourceReader.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using UnityEngine;
+
+namespace Assets.Scripts.Localization
+{
+    public class ResourceReader
+    {
+        private const string XPathLanguageTemplate = "/Languages/{0}";
+        private const string AttributeName = "name";
+        private const SystemLanguage DefaultLanguage = SystemLanguage.English;
+        public static string ResourceFileName = Path.Combine("Scripts\\Localization", "Resources.xml");
+        private readonly IDictionary<string, string> _resources;
+        public ResourceReader(string resourceFilePath, SystemLanguage language)
+        {
+            XmlDocument xml = new XmlDocument();
+            _resources = new Dictionary<string, string>();
+            xml.Load(resourceFilePath);
+            if (xml.DocumentElement != null)
+            {
+                XmlNode languageNode = xml.DocumentElement.SelectSingleNode(string.Format(XPathLanguageTemplate, language));
+
+                if (languageNode != null)
+                {
+                    foreach (XmlNode languageNodeChildNode in languageNode.ChildNodes)
+                    {
+                        if (languageNodeChildNode != null && languageNodeChildNode.Attributes != null &&
+                            languageNodeChildNode.Attributes[AttributeName] != null)
+                        {
+                            _resources.Add(languageNodeChildNode.Attributes[AttributeName].InnerText,
+                                languageNodeChildNode.InnerText);
+                        }
+                    }
+                }
+                else
+                {
+                    //language not found, using default = english
+                    languageNode = xml.DocumentElement.SelectSingleNode(string.Format(XPathLanguageTemplate, DefaultLanguage));
+                    if (languageNode != null)
+                    {
+                        foreach (XmlNode languageNodeChildNode in languageNode.ChildNodes)
+                        {
+                            if (languageNodeChildNode != null && languageNodeChildNode.Attributes != null &&
+                                languageNodeChildNode.Attributes[AttributeName] != null)
+                            {
+                                _resources.Add(languageNodeChildNode.Attributes[AttributeName].InnerText,
+                                    languageNodeChildNode.InnerText);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public string GetResource(string key)
+        {
+            string result;
+            try
+            {
+                result = _resources[key];
+            }
+            catch (KeyNotFoundException)
+            {
+                result = string.Empty;
+            }
+            return result;
+        }
+    }
+}
