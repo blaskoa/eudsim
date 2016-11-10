572de50e015fce782fc55f1797542dddee998b2a
Bloodxy <gulis2505@gmail.com>
2016-11-10 21:41:39 +0100

    Changes in code for code review, fixed bug with missing selectionboxes.

--- Assets/Scripts/GenericScripts/DebugDisplay.cs
+++ Assets/Scripts/GenericScripts/DebugDisplay.cs
@@ -4,18 +4,14 @@ using UnityEngine.UI;
  
 public class DebugDisplay : MonoBehaviour 
 {
-	//Debug log text Component.
+	
+    //Debug log text Component.
     public Text output;
-	
-    void Start () 
-	{
-    }
-	
-	//Function to write argument message into DebugLog and scroll down for the fresh line.
+
+    //Function to write argument message into DebugLog and scroll down for the fresh line.
     public void Write(string arg0)
     {
         output.text += arg0 + "\n";
-		this.gameObject.GetComponent<ScrollRect>().velocity = new Vector2(0f,1000f);
-    }     
- 
+        this.gameObject.GetComponent<ScrollRect>().velocity = new Vector2(0f,1000f);
+    } 
 }
--- Assets/Scripts/GenericScripts/Whisp.cs
+++ Assets/Scripts/GenericScripts/Whisp.cs
@@ -5,23 +5,22 @@ using UnityEngine.UI;
 public class Whisp : MonoBehaviour 
 {
  
-	private DebugDisplay debugLog;
-	private GameObject dl;
-	/*
-	Object could call this from their functions by:
-	this.GetComponent<Whisp>().Say("Hello from the other side.");
-	*/
-	void Start()
+    private DebugDisplay debugLog;
+    private GameObject dl;
+    /*
+    Object could call this from their functions by:
+    this.GetComponent<Whisp>().Say("Hello from the other side.");
+    */
+    void Start()
     {
-		dl = GameObject.Find("DebugLog");
-		debugLog = (DebugDisplay) dl.GetComponent(typeof(DebugDisplay));
-		Say("Hello, I am " + this.gameObject.name + "! :)");
+        dl = GameObject.Find("DebugLog");
+        debugLog = (DebugDisplay) dl.GetComponent(typeof(DebugDisplay));
+        Say("Hello, I am " + this.gameObject.name + "! :)");
     }
-	
-	//Send message into debug log.
+
+    //Send message into debug log.
     public void Say (string arg0) 
-	{
-		debugLog.Write(arg0);
-    }   
- 
+    {
+        debugLog.Write(arg0);
+    } 
 }
