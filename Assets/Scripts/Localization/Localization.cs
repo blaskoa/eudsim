131b07dfd68a1b5db18049c2e2320f443e078d90
blaskoa <adam.blasko1@gmail.com>
2016-11-11 20:26:29 +0100

    Code refactoring, resolved review issues, slightly altered logic

--- Assets/Scripts/Localization/Localization.cs
+++ Assets/Scripts/Localization/Localization.cs
@@ -2,26 +2,36 @@
 using Assets.Scripts.Localization;
 using UnityEngine;
 using UnityEngine.UI;
-
+// Component is meant to be attatched to root element of the whole GUI (canvas)
 public class Localization : MonoBehaviour
 {
     private ResourceReader _resourceReader;
-	// Use this for initialization
-	void Start ()
+    private int _languageCycle;
+    // instantiate Resource reader and set text elements text from resrouce file
+    void Start()
     {
-	    _resourceReader = new ResourceReader(Path.Combine(Application.dataPath, ResourceReader.ResourceFileName), Application.systemLanguage);
-        GetComponent<Text>().text = _resourceReader.GetResource(gameObject.name);
+        _resourceReader = new ResourceReader(Path.Combine(Application.dataPath, ResourceReader.ResourceFileName),
+            Application.systemLanguage);
+        SetElementTexts();
     }
-
+    // used for testing purposes - cycles language between Slovak and English
+    public void CycleLanguage()
+    {
+        ChangeLanguage(_languageCycle++%2 == 0 ? SystemLanguage.Slovak : SystemLanguage.English);
+    }
+    // create a new instance of resource reader and set text elements text from resrouce file
     public void ChangeLanguage(SystemLanguage language)
     {
-        _resourceReader = new ResourceReader(Path.Combine(Application.dataPath, ResourceReader.ResourceFileName), language);
-        GetComponent<Text>().text = _resourceReader.GetResource(gameObject.name);
+        _resourceReader = new ResourceReader(Path.Combine(Application.dataPath, ResourceReader.ResourceFileName),
+            language);
+        SetElementTexts();
     }
-	
-	// Update is called once per frame
-	void Update ()
+    // cycles trough all child elements and sets their texts according to current language
+    private void SetElementTexts()
     {
-	
-	}
+        foreach (Text textComponent in GetComponentsInChildren<Text>(true))
+        {
+            textComponent.text = _resourceReader.GetResource(textComponent.name);
+        }
+    }
 }
--- Assets/Scripts/Localization/ResourceReader.cs
+++ Assets/Scripts/Localization/ResourceReader.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 
 namespace Assets.Scripts.Localization
 {
+    // Class that parses XML resource file and generates a string dictionary with localized values
+    // text element names are used as keys
     public class ResourceReader
     {
         private const string XPathLanguageTemplate = "/Languages/{0}";
         private const string AttributeName = "name";
         private const SystemLanguage DefaultLanguage = SystemLanguage.English;
-        public static string ResourceFileName = Path.Combine("Scripts\\Localization", "Resources.xml");
         private readonly IDictionary<string, string> _resources;
+        public static string ResourceFileName = Path.Combine("Scripts\\Localization", "Resources.xml");
+        
         public ResourceReader(string resourceFilePath, SystemLanguage language)
         {
             XmlDocument xml = new XmlDocument();
@@ -20,40 +23,27 @@ namespace Assets.Scripts.Localization
             xml.Load(resourceFilePath);
             if (xml.DocumentElement != null)
             {
-                XmlNode languageNode = xml.DocumentElement.SelectSingleNode(string.Format(XPathLanguageTemplate, language));
+                // selects requested laguage node if requested language is not found, use default = english
+                XmlNode languageNode = xml.DocumentElement.SelectSingleNode(string.Format(XPathLanguageTemplate, language)) ??
+                                       xml.DocumentElement.SelectSingleNode(string.Format(XPathLanguageTemplate, DefaultLanguage));
 
                 if (languageNode != null)
                 {
+                    // iterates through child nodes of language nodes
                     foreach (XmlNode languageNodeChildNode in languageNode.ChildNodes)
                     {
                         if (languageNodeChildNode != null && languageNodeChildNode.Attributes != null &&
                             languageNodeChildNode.Attributes[AttributeName] != null)
                         {
+                            // fills the resource dictionary with name attribute as key and element text value as value
                             _resources.Add(languageNodeChildNode.Attributes[AttributeName].InnerText,
                                 languageNodeChildNode.InnerText);
                         }
                     }
                 }
-                else
-                {
-                    //language not found, using default = english
-                    languageNode = xml.DocumentElement.SelectSingleNode(string.Format(XPathLanguageTemplate, DefaultLanguage));
-                    if (languageNode != null)
-                    {
-                        foreach (XmlNode languageNodeChildNode in languageNode.ChildNodes)
-                        {
-                            if (languageNodeChildNode != null && languageNodeChildNode.Attributes != null &&
-                                languageNodeChildNode.Attributes[AttributeName] != null)
-                            {
-                                _resources.Add(languageNodeChildNode.Attributes[AttributeName].InnerText,
-                                    languageNodeChildNode.InnerText);
-                            }
-                        }
-                    }
-                }
             }
         }
-
+        // gets resource by key passed as argument
         public string GetResource(string key)
         {
             string result;
