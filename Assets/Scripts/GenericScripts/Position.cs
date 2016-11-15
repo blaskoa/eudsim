c229fc3a86abadfd41ace4c3b664bddf6eb4bb62
Martin Cvicela <mato155@gmail.com>
2016-11-15 16:25:54 +0100

    hotfix suciastok

---
+++ Assets/Scripts/GenericScripts/Position.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour
+{
+
+    private Vector3 a;
+
+	// Use this for initialization
+	void Start ()
+	{
+	     a = Input.mousePosition;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Debug.Log(a.x + " " + a.y + " " + a.z);
+	}
+}
