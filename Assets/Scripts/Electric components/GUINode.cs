fa19c4c11fadd958874757f499e1b55cc9c69e37
Marek Matula <marekmatula126@gmail.com>
2016-11-18 10:17:43 +0100

    Added simulation between Battery and Bulb. Added GUINode element

---
+++ Assets/Scripts/Electric components/GUINode.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using ClassLibrarySharpCircuit;
+using leader = ClassLibrarySharpCircuit.Circuit.Lead;
+
+public class GUINode : Component2
+{
+    public Resistor MyComponent;
+
+    // Use this for initialization
+    public void Start()
+    {
+        if (this.CompareTag("ActiveItem"))
+        {
+            //connectors = GetComponentsInChildren<Connector>();
+            connectors[0] = gameObject.AddComponent<Connector>();
+            connectors[0].setConnectedConnectors();
+            connectors[0].assignComponent(this);
+        }
+    }
+}
--- Assets/Scripts/GUICircuit.cs
+++ Assets/Scripts/GUICircuit.cs
@@ -12,31 +12,7 @@ public class GUICircuit : MonoBehaviour
     public void Sim0()
     {
         //simulacia obvodu
-
-        /*  
-         *  TENTO KOD JE DO BUDUCNA POTREBNY, AVSAK PRE JEDNODUCHSIE DEBUGOVANIE HO ZANEDBAME A ZATIAL VYTVARAME CONNECTIONS PRIAMO PRI CIARACH
-         *  
-         *   GUIBattery battery = gameObject.AddComponent<GUIBattery>();
-           GUIResistor res0 = gameObject.AddComponent<GUIResistor>();
-           GUIResistor res1 = gameObject.AddComponent<GUIResistor>();
-
-           battery.Start();
-           res0.Start();
-           res1.Start();
-
-           sim.Connect(battery.DLLConnectors[0], res0.DLLConnectors[0]);
-           sim.Connect(res0.DLLConnectors[1], res1.DLLConnectors[0]);
-           sim.Connect(res1.DLLConnectors[1], battery.DLLConnectors[1]);
-
-           for (int x = 1; x <= 10; x++)
-           {
-               sim.doTick();
-
-               Debug.Log("battery voltage " + battery.MyComponent.maxVoltage + "res voltage" + res0.MyComponent.getVoltageDelta()); // V = I x R
-           }*/
-
         Debug.Log("Simulation complete");
-
     }
 
     // funkcia bez popisu, nepouzita
@@ -60,65 +36,32 @@ public class GUICircuit : MonoBehaviour
 
     void SimulationFlow() // tato funkcia zavola objekty so scenky, prepoji ich umelo, zavola algoritmus na zrusenie uzlov, prepoji zoznamy dllconectorov a spusti simulaciu
     {
-        Component2[] listOfComponents = new Component2[2];
-        /*  
-         *  TENTO KOD JE DO BUDUCNA POTREBNY, AVSAK PRE JEDNODUCHSIE DEBUGOVANIE HO ZANEDBAME A ZATIAL VYTVARAME CONNECTIONS PRIAMO PRI CIARACH
-         *  
-                GUIBattery battery = (GUIBattery)UnityEngine.Object.FindObjectsOfType(typeof(GUIBattery))[0];
-                GUILamp lamp = (GUILamp)UnityEngine.Object.FindObjectsOfType(typeof(GUILamp))[0];
-
-                Connector uzol = null;
-                foreach (Connector obj in (Connector[])UnityEngine.Object.FindObjectsOfType(typeof(Connector)))
-                {
-                    if (obj.transform.parent == null)  // uzol nema rodica
-                    {
-                        uzol = obj;
-                    }
-                }
-
-                uzol.setConnectedConnectors();
-                Debug.Log(battery.connectors[0].ConnectedConnectors.Length);
-
-                battery.connectors[0].ConnectedConnectors[0] = uzol;         //prvy konector baterky ma neinicializovany list konectorov ku ktorym sa pripaja
-                lamp.connectors[0].ConnectedConnectors[0] = uzol;
-
-                uzol.ConnectedConnectors[0] = battery.connectors[0];
-                uzol.ConnectedConnectors[1] = lamp.connectors[1];
-
-                listOfComponents[0] = battery;
-                listOfComponents[1] = lamp;*/
+        Component2[] listOfComponents = new Component2[4];
 
         GraphAlgorithm algorithm = new GraphAlgorithm();
         sceneItems.CopyTo(listOfComponents, 0);
-        /*  
-         *  TENTO KOD JE DO BUDUCNA POTREBNY, AVSAK PRE JEDNODUCHSIE DEBUGOVANIE HO ZANEDBAME A ZATIAL VYTVARAME CONNECTIONS PRIAMO PRI CIARACH
-         *  
-         *  ConnectionsOfComponent[] dllconnectionsOfComponents = algorithm.untangle(listOfComponents);
+        /*   ConnectionsOfComponent[] dllconnectionsOfComponents = algorithm.untangle(listOfComponents);
 
-          Debug.Log("-------pocet komponentov" + listOfComponents.Length);
+           Debug.Log(dllconnectionsOfComponents.Length);
 
-          Debug.Log("Tu si sa dostal");
+           //sim.Connect(dllconnectionsOfComponents[0].dllconnections[0].connectedDllconnectors[0], dllconnectionsOfComponents[0].dllconnections[0].dllconector);
 
-          Debug.Log(dllconnectionsOfComponents.Length);
-
-          //sim.Connect(dllconnectionsOfComponents[0].dllconnections[0].connectedDllconnectors[0], dllconnectionsOfComponents[0].dllconnections[0].dllconector);
-
-          for (int i = 0; i < dllconnectionsOfComponents.Length; i++)
-          {
-              for (int a = 0; a < dllconnectionsOfComponents[i].dllconnections.Length; a++)
-              {
-                  for (int b = 0; b < dllconnectionsOfComponents[i].dllconnections[a].connectedDllconnectors.Length; b++)
-                  {
-                      if (dllconnectionsOfComponents[i].dllconnections[a].dllconector != dllconnectionsOfComponents[i].dllconnections[a].connectedDllconnectors[b])
-                      {
-                          Debug.Log(i + a + b);
-                          //sim.Connect(dllconnectionsOfComponents[i].dllconnections[a].dllconector, dllconnectionsOfComponents[i].dllconnections[a].connectedDllconnectors[b]);
-                          countOfMadeConnections+=1;
-                      }
-                  }
-              }
-          }*/
-
+           for (int i = 0; i < dllconnectionsOfComponents.Length; i++)
+           {
+               for (int a = 0; a < dllconnectionsOfComponents[i].dllconnections.Length; a++)
+               {
+                   for (int b = 0; b < dllconnectionsOfComponents[i].dllconnections[a].connectedDllconnectors.Length; b++)
+                   {
+                       if (dllconnectionsOfComponents[i].dllconnections[a].dllconector != dllconnectionsOfComponents[i].dllconnections[a].connectedDllconnectors[b])
+                       {
+                           Debug.Log(i + a + b);
+                           //sim.Connect(dllconnectionsOfComponents[i].dllconnections[a].dllconector, dllconnectionsOfComponents[i].dllconnections[a].connectedDllconnectors[b]);
+                           countOfMadeConnections += 1;
+                       }
+                   }
+               }
+           }
+           */
         Debug.Log("Simulation Begin with" + countOfMadeConnections + "connections");
         Debug.Log("Sim Elements count " + sim.elements.Count);
 
@@ -126,6 +69,14 @@ public class GUICircuit : MonoBehaviour
         for (int x = 1; x <= 1; x++)
         {
             sim.doTick();
+            Debug.Log(sceneItems.Count + "komponentov");
+            for (int i = 0; i < sceneItems.Count; i++)
+            {
+                if (listOfComponents[i].GetType() == typeof(GUIBattery))
+                    Debug.Log("Ja SOM BATERKA");
+                if (listOfComponents[i].GetType() == typeof(GUIResistor))
+                    Debug.Log("Ja SOM RESISTOR");
+            }
 
             //  TENTO KOD JE DO BUDUCNA POTREBNY, AVSAK PRE JEDNODUCHSIE DEBUGOVANIE HO ZANEDBAME A ZATIAL VYTVARAME CONNECTIONS PRIAMO PRI CIARACH
             //  Debug.Log("battery voltage " + battery.MyComponent.getVoltageDelta() + "lamp voltage" + lamp.MyComponent.getVoltageDelta()); // V = I x R
