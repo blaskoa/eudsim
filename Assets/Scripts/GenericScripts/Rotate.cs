43af1201625a1f744861e9109d2057ee147df2b4
blaskoa <adam.blasko1@gmail.com>
2016-12-09 22:29:26 +0100

    Change usage of hotkeys for known classes

--- Assets/Scripts/GenericScripts/Rotate.cs
+++ Assets/Scripts/GenericScripts/Rotate.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
+using Assets.Scripts.Hotkeys;
 
-public class Rotate : MonoBehaviour {
-
+public class Rotate : MonoBehaviour
+{
+    public const string RotateLeftHotkeyKey = "RotateLeft";
+    public const string RotateRightHotkeyKey = "RotateRight";
     // Update is called once per frame
     void Update () {
         // Check if any object is selected.
         if (SelectObject.SelectedObject != null && this.gameObject == SelectObject.SelectedObject)
         {
             // Check if Q is pressed.
-            if (Input.GetKey("q"))
+            if (HotkeyManager.Instance.CheckHotkey(RotateLeftHotkeyKey))
             {
                 transform.Rotate(new Vector3(0, 0, Time.deltaTime * 100));
             }
             // Check if E is pressed.
-            else if (Input.GetKey("e"))
+            else if (HotkeyManager.Instance.CheckHotkey(RotateRightHotkeyKey))
             {
                 transform.Rotate(new Vector3(0, 0,  - Time.deltaTime * 100));
             }
