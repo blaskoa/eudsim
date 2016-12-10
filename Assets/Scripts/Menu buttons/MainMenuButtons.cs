8802e391ee3b1054b395c4bf4c636637cbddf069
Bloodxy <gulis2505@gmail.com>
2016-12-10 13:16:53 +0100

    New mechanism for closing/opening panels created (script). Main menu edited to be ready for next tasks (wider, align). New buttons added: rotate left and right, delete component, exit(in menu), delete(in menu). New panel for New Project created. Added new resources.

---
+++ Assets/Scripts/Menu buttons/MainMenuButtons.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	private bool _NewProjectCanvasOpen = false;
+	private bool _FilePanelMenuOpen = false;
+	private bool _EditPanelMenuOpen = false;
+	private bool _ViewPanelMenuOpen = false;
+	private bool _HelpPanelMenuOpen = false;
+	
+	public void FilePanelMenu(GameObject obj)
+    {		
+	    _FilePanelMenuOpen = obj.active;
+		
+	    if (_FilePanelMenuOpen == false)
+	    {
+	        _FilePanelMenuOpen = true;
+	        obj.SetActive(true);
+	    } else
+	    {
+	        _FilePanelMenuOpen = false;
+	        obj.SetActive(false);
+	    }
+    }
+	
+	public void EditPanelMenu(GameObject obj)
+    {		
+	    _EditPanelMenuOpen = obj.active;
+		
+	    if (_EditPanelMenuOpen == false)
+	    {
+	        _EditPanelMenuOpen = true;
+	        obj.SetActive(true);
+	    } else
+	    {
+	        _EditPanelMenuOpen = false;
+	        obj.SetActive(false);
+	    }
+    }
+	
+	public void ViewPanelMenu(GameObject obj)
+    {		
+	    _ViewPanelMenuOpen = obj.active;
+		
+	    if (_ViewPanelMenuOpen == false)
+	    {
+	        _ViewPanelMenuOpen = true;
+	        obj.SetActive(true);
+	    } else
+	    {
+	        _ViewPanelMenuOpen = false;
+	        obj.SetActive(false);
+	    }
+    }
+	
+	public void HelpPanelMenu(GameObject obj)
+    {		
+	    _HelpPanelMenuOpen = obj.active;
+		
+	    if (_HelpPanelMenuOpen == false)
+	    {
+	        _HelpPanelMenuOpen = true;
+	        obj.SetActive(true);
+	    } else
+	    {
+	        _HelpPanelMenuOpen = false;
+	        obj.SetActive(false);
+	    }
+    }
+	
+    public void NewProjectCanvas(GameObject obj)
+    {		
+	    _NewProjectCanvasOpen = obj.active;
+		
+	    if (_NewProjectCanvasOpen == false)
+	    {
+	        _NewProjectCanvasOpen = true;
+	        obj.SetActive(true);
+	    } else
+	    {
+	        _NewProjectCanvasOpen = false;
+	        obj.SetActive(false);
+	    }
+    }
+}
