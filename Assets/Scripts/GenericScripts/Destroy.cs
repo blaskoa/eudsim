2ca8f799b34e4679e3194721b135596390d1b55f
Marek Matula <marekmatula126@gmail.com>
2016-12-11 22:07:14 +0100

    delete changed according to review - removed redundant code

--- Assets/Scripts/GenericScripts/Destroy.cs
+++ Assets/Scripts/GenericScripts/Destroy.cs
@@ -2,7 +2,8 @@
 using UnityEngine;
 
 
-public class Destroy : MonoBehaviour {
+public class Destroy : MonoBehaviour
+{
 
     public void DeleteSelected()
     {
@@ -58,67 +59,6 @@ public class Destroy : MonoBehaviour {
 
         }
 
-        // Destroy selected line when delete key was pressed 
-        if (SelectObject.SelectedObject != null &&
-            (Line.SelectedLine != null && SelectObject.SelectedObject.gameObject == Line.SelectedLine) 
-            && SelectObject.SelectedObject.tag.Equals("ActiveItem"))
-        {
-            // Delete connected connectors from lists of connectors
-            SelectObject.SelectedObject.gameObject.GetComponent<Line>()
-                .Begin.GetComponent<Connectable>()
-                .Connected.Remove(SelectObject.SelectedObject.gameObject.GetComponent<Line>().End.gameObject);
-            SelectObject.SelectedObject.gameObject.GetComponent<Line>()
-                .End.GetComponent<Connectable>()
-                .Connected.Remove(SelectObject.SelectedObject.gameObject.GetComponent<Line>().Begin.gameObject);
-            Destroy(SelectObject.SelectedObject.gameObject);
-        }
-       
-    }
-
-    // Update is called once per frame
-    void Update () {
-        if (SelectObject.SelectedObject != null && this.gameObject == SelectObject.SelectedObject)
-        {
-            if (Input.GetKey(KeyCode.Delete))
-            {
-                // List connected connectors with plusconnector
-                List<GameObject> connected1 = this.gameObject.transform.GetChild(0).GetComponent<Connectable>().Connected;
-
-                // List connected connectors with minusconnector
-                List<GameObject> connected2 = this.gameObject.transform.GetChild(1).GetComponent<Connectable>().Connected; 
-
-                // First update list of connected connectors in connected component with this component
-                foreach (GameObject c in connected1)
-                {
-                    c.gameObject.GetComponent<Connectable>().Connected.Remove(this.gameObject.transform.GetChild(0).gameObject);
-                }
-
-                foreach (GameObject c in connected2)
-                {
-                    c.gameObject.GetComponent<Connectable>().Connected.Remove(this.gameObject.transform.GetChild(1).gameObject);
-                }
-
-                // For each lines in scene
-                GameObject[] objs = GameObject.FindGameObjectsWithTag("Line");
-
-                foreach (GameObject currentLine in objs)
-                {
-                    // Except parental line
-                    if (currentLine.transform.name != "Line")
-                    {
-                        // For every line connected to this component
-                        if (this.gameObject.transform.GetChild(0).gameObject == currentLine.GetComponent<Line>().Begin 
-                            || this.gameObject.transform.GetChild(1).gameObject == currentLine.GetComponent<Line>().Begin
-                            || this.gameObject.transform.GetChild(0).gameObject == currentLine.GetComponent<Line>().End
-                            || this.gameObject.transform.GetChild(1).gameObject == currentLine.GetComponent<Line>().End)
-                        {                            
-                            Destroy(currentLine.gameObject);
-                        }                       
-                    }
-                }
-                Destroy(this.gameObject);
-            }
-        }
 
         // Destroy selected line when delete key was pressed 
         if (Line.SelectedLine != null && this.gameObject == Line.SelectedLine)
@@ -130,6 +70,16 @@ public class Destroy : MonoBehaviour {
                 this.gameObject.GetComponent<Line>().End.GetComponent<Connectable>().Connected.Remove(this.gameObject.GetComponent<Line>().Begin.gameObject);
                 Destroy(this.gameObject);
             }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Delete))
+        {
+            DeleteSelected();
         }
     }
 }
