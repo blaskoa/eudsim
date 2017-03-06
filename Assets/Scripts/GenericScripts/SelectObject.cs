67590c00d642973d7d10935dfc1135210f4d0395
Martin Cvicela <mato155@gmail.com>
2017-03-06 09:32:53 +0100

    no message

--- Assets/Scripts/GenericScripts/SelectObject.cs
+++ Assets/Scripts/GenericScripts/SelectObject.cs
@@ -3,18 +3,13 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 using System;
-using System.Collections.Generic;
 
 public class SelectObject : MonoBehaviour, IPointerClickHandler
-{   
+{
+    
     // Global variable to allow only one selected item.
+    public static GameObject SelectedObject;
     public GameObject SelectionBox;
-    public static List<GameObject> SelectedObjects = new List<GameObject>();
-    public static List<GameObject> SelectedLines = new List<GameObject>();
-    private EditObjectProperties _script;
-
-    //util class for work with toolbar buttons 
-    private ToolbarButtonUtils _tbu = new ToolbarButtonUtils();
 
     // Initialization: Making object and SelectionBox the same size.
     void Start ()
@@ -22,65 +17,52 @@ public class SelectObject : MonoBehaviour, IPointerClickHandler
         SelectionBox.transform.position = this.transform.position;
         SelectionBox.transform.localScale = this.transform.localScale;
         SelectionBox.GetComponent<SpriteRenderer>().enabled = false;
-        GameObject propertiesContainer = GameObject.Find("PropertiesWindowContainer");
-        _script = propertiesContainer.GetComponent<EditObjectProperties>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
-    {
-            
-        if (this.gameObject.tag == "ToolboxItem" || this.gameObject.tag == "Node" || this.gameObject.tag == "ToolboxItemActive")
+    {        
+        GameObject propertiesContainer = GameObject.Find("PropertiesWindowContainer");
+        EditObjectProperties script = propertiesContainer.GetComponent<EditObjectProperties>();
+
+        if (this.gameObject.tag == "ToolboxItem" || this.gameObject.tag == "Node")
         {
             return;
         }
 
         // Deselect selected item first.
-        if (!SelectedObjects.Contains(this.gameObject))
+        if (SelectedObject != null)
         {
-            DeselectObject();
+            SelectedObject.transform.FindChild("SelectionBox").GetComponent<SpriteRenderer>().enabled = false;
+            SelectedObject = this.gameObject;
         }
 
         // Deselect line
-        if (!SelectedObjects.Contains(this.gameObject))
+        if (Line.SelectedLine != null)
         {
-            GameObject line = GameObject.Find("Line(Clone)");
-            if (line != null)
-            {
-                line.GetComponent<Line>().DeselectLine();
-            }
+            Line.SelectedLine.GetComponent<LineRenderer>().SetColors(Color.black, Color.black);
+            Line.SelectedLine = null;
+            script.Clear();
         }
 
-        // Select new object. 
-        if (!SelectedObjects.Contains(this.gameObject))
-        {
-            SelectedObjects.Add(this.gameObject);
-            SelectionBox.GetComponent<SpriteRenderer>().enabled = true;
-        }
-
-        _tbu.EnableToolbarButtons();
+        // Select new object.
+        SelectedObject = this.gameObject;
+        SelectionBox.GetComponent<SpriteRenderer>().enabled = true;
+		
+		// Enable buttons for component manipulation
+        GameObject rotateLeftButton = GameObject.Find("RotateLeftButton");
+        GameObject rotateRightButton = GameObject.Find("RotateRightButton");
+        GameObject deleteButton = GameObject.Find("DeleteButton");
+        GameObject menuDeleteButton = GameObject.Find("MenuDeleteButton");
+        rotateLeftButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
+        rotateRightButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
+        deleteButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
+        menuDeleteButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
 
         // Clear the Properties Window
-        _script.Clear();
-       
-        // Call the script from component that fills the Properties Window
-        if (SelectedObjects.Count == 1)
-        {
-            GUICircuitComponent componentScript = SelectedObjects[0].GetComponent<GUICircuitComponent>();
-            componentScript.GetProperties();
-        }
-    }
+        script.Clear();
 
-    public void DeselectObject()
-    {        
-        //deselect component
-        if (SelectedObjects.Count != 0)
-        {
-            foreach (GameObject objectSelected in SelectedObjects)
-            {
-                objectSelected.transform.FindChild("SelectionBox").GetComponent<SpriteRenderer>().enabled = false;
-            }
-            SelectedObjects.Clear();
-            _script.Clear();
-        }
+        // Call the script from component that fills the Properties Window
+        GUICircuitComponent componentScript = SelectedObject.GetComponent<GUICircuitComponent>();
+        componentScript.GetProperties();
     }
 }
