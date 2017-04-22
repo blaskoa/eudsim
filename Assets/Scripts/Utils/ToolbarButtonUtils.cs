9f3564595e22fa9fd3a8ed22deb89e932b2d2637
Bloodxy <gulis2505@gmail.com>
2017-04-22 14:46:28 +0200

    Fixed mouse over for most of toolbar buttons, added duplicate and manual buttons.

--- Assets/Scripts/Utils/ToolbarButtonUtils.cs
+++ Assets/Scripts/Utils/ToolbarButtonUtils.cs
@@ -12,10 +12,12 @@ public class ToolbarButtonUtils
         GameObject rotateRightButton = GameObject.Find("RotateRightButton");
         GameObject deleteButton = GameObject.Find("DeleteButton");
         GameObject menuDeleteButton = GameObject.Find("MenuDeleteButton");
+        GameObject duplicateButton = GameObject.Find("DuplicateButton");
         rotateLeftButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
         rotateRightButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
         deleteButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
         menuDeleteButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
+        duplicateButton.GetComponent<UnityEngine.UI.Button>().interactable = true;
     }
 
     public void DisableToolbarButtons()
@@ -25,10 +27,12 @@ public class ToolbarButtonUtils
         GameObject rotateRightButton = GameObject.Find("RotateRightButton");
         GameObject deleteButton = GameObject.Find("DeleteButton");
         GameObject menuDeleteButton = GameObject.Find("MenuDeleteButton");
+        GameObject duplicateButton = GameObject.Find("DuplicateButton");
         rotateLeftButton.GetComponent<UnityEngine.UI.Button>().interactable = false;
         rotateRightButton.GetComponent<UnityEngine.UI.Button>().interactable = false;
         deleteButton.GetComponent<UnityEngine.UI.Button>().interactable = false;
         menuDeleteButton.GetComponent<UnityEngine.UI.Button>().interactable = false;
+        duplicateButton.GetComponent<UnityEngine.UI.Button>().interactable = false;
     }
 
 }
